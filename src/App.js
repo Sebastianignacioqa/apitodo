bff86bd3f7db7d79d488a988c6cb75b85d2f55ae
Sebastianignacioqa <sebastianignacioqa@gmail.com>
2021-08-31 20:09:38 -0400

    Set fetch

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { useState, useEffect } from 'react';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+
+  const [lista, setLista] = useState([]);
+
+  useEffect(() => {
+
+    cargarInfo()
+
+  }, []);
+
+    const cargarInfo = () => {
+
+    fetch("https://assets.breatheco.de/apis/fake/todos/user/sebastianignacioqa", {
+      method: "GET",
+      headers: { "Content-Type": "application/json" }
+    }).then(response =>
+      response.json())
+
+      .then(data => 
+
+      setLista(data.map(t => t.label)))
+
+      .catch(error => console.log(error))
+    };
+  
+
+  return (<ul>
+    <li>{lista[0]}</li>
+    <li>{lista[1]}</li>
+    <li>{lista[2]}</li>
+    <li>{lista[3]}</li>
+  </ul>)
+
+};
 
 export default App;
